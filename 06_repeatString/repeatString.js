8012d8e326bbfd1582a04d12000e6776a6f6bc2c
JakobKrizaj <jakob.krizaj08@gmail.com>
2025-09-14 17:29:59 +0200

    Complete exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finalString = "";
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+        finalString = finalString + string; 
+        }
+        return finalString;
+    } else {
+        return "ERROR";
+    }
+    
 };
+console.log(repeatString("hey", 3));
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
+
